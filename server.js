79eea2f70d5b46fa74ba436239128000e60b8cb2
Anže Jenšterle <jensterle.anze@gmail.com>
2016-11-26 23:09:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var slapp = Slapp({
 slapp.command('/nmap', (msg, domain) => {
   // `respond` is used for actions or commands and uses the `response_url` provided by the
   // incoming request from Slack
-  msg.respond(`https://dnsdumpster.com/static/map/${domain}.png`)
+  msg.say(`https://dnsdumpster.com/static/map/${domain}.png`)
 })
 
 // attach Slapp to express server
